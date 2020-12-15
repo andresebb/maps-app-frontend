fa4d2570730713d535dfe2a53b4519643918efb8
Andres Betancourt <aebb001@gmail.com>
2020-12-15 12:11:46 -0400

    Referencia de lng, lat y zoom cuando se mueve el mapa

--- src/pages/MapaPage.js
+++ src/pages/MapaPage.js
@@ -5,8 +5,8 @@ mapboxgl.accessToken =
   "pk.eyJ1IjoiY2hpZ3VpcmVjb2RlIiwiYSI6ImNraXEyazBjNTBhYzUyc2xyb2kweno1eGcifQ.j8cpfzTNCCIdcDRXl5xz9w";
 
 const puntoInicial = {
-  lng: -70.26212021970849,
-  lat: 8.620686980291502,
+  lng: -70.2621,
+  lat: 8.6206,
   zoom: 15,
 };
 
@@ -14,7 +14,9 @@ export const MapaPage = () => {
   const mapaDiv = useRef();
 
   const [mapa, setMapa] = useState();
+  const [cordenadas, setCordenadas] = useState(puntoInicial);
 
+  // Cargamos el mapa
   useEffect(() => {
     const map = new mapboxgl.Map({
       container: mapaDiv.current,
@@ -23,11 +25,36 @@ export const MapaPage = () => {
       zoom: puntoInicial.zoom,
     });
 
+    map.addControl(new mapboxgl.NavigationControl());
+
+    var marker = new mapboxgl.Marker()
+      .setLngLat([cordenadas.lng, cordenadas.lat])
+      .addTo(map);
+
     setMapa(map);
   }, []);
 
+  // Cuando se mueve el mapa
+  useEffect(() => {
+    if (mapa !== undefined) {
+      mapa.on("move", () => {
+        const { lng, lat } = mapa.getCenter();
+        setCordenadas({
+          lng: lng.toFixed(4),
+          lat: lat.toFixed(4),
+          zoom: mapa.getZoom().toFixed(2),
+        });
+      });
+
+      // return mapa.off("move");
+    }
+  }, [mapa]);
+
   return (
     <>
+      <div className="info">
+        long {cordenadas.lng} | lat {cordenadas.lat} | zoom {cordenadas.zoom}
+      </div>
       <div ref={mapaDiv} className="mapContainer" />
     </>
   );
